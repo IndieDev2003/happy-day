b4a46465766093010ddea090a257b5f388e075d9
Gagan Suman <fbdgagansuman@gmail.com>
2024-12-11 21:01:48 +0530

    Update text.js

--- src/assets/text.js
+++ src/assets/text.js
@@ -1,14 +1,14 @@
 export default {
   main: {
-    heading: "", //max 10 words
-    para: "", // max 60 words
+    heading: "HAPPY BIRTHDAY MAU", //max 10 words
+    para: "Yayy its my catty's birthday me loves my catty alot and my catty is the person who has given meaning to my life my catty is everything for me but i want my catty to remmeber one thing that me is always on his side and me will support him always.", // max 60 words
   },
   sec1: {
-    heading: "", // max 10 words
-    para: "", // max 60 words
+    heading: "MISSION: DON'T BE SAD CATTY", // max 10 words
+    para: "I don't know why my catty is always so sad I like my catty to be happy because he looks very cute when catty is happy . Its okay catty if nobody else wishes you on your birthday its okay me will wish you and me will spend time with you on your birthday.", // max 60 words
   },
   sec2: {
-    heading: "", // max 10 words
-    para: "", // max 60 words
+    heading: "MY PROMISE TO CATTY", // max 10 words
+    para: "Me is promising my catty that when me will be with catty me will make every birthday memorable for him and he won't be sad but until then me wants catty to understand me and be with me , me will try to make his birthday every year memorable this is my promise to my catty because catty menans the world to me CATTY YOU ARE MY WORLD.", // max 60 words
   },
 };
