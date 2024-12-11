fac6f3658acbf416a2ab3f212b44374d1b37bffd
Gagan Suman <fbdgagansuman@gmail.com>
2024-12-11 15:47:29 +0530

    hero

--- src/components/Hero/Hero.jsx
+++ src/components/Hero/Hero.jsx
@@ -3,29 +3,39 @@ import gsap from "gsap";
 import React from "react";
 
 function Hero() {
+  useGSAP(() => {
+    const tl = gsap.timeline();
 
-
-    useGSAP(() => {
-        const tl = gsap.timeline();
-
-        tl.from(".hero-h2", {
-            y: 100,
-            opacity: 0,
-            duration:1
-        })
-     }, {
-        
-    })
+    tl.from(".hero-h2", {
+      y: 100,
+      opacity: 0,
+      duration: 1,
+    });
+    tl.from(".hero-p", {
+      x: 100,
+      opacity: 0,
+      duration: 1,
+    });
+    tl.from(".hero-btn", {
+      y: -20,
+      opacity: 0,
+      duration: 1,
+    });
+  }, {});
 
   return (
-    <div className="min-h-screen w-screem flex items-center justify-center text-black dark:text-white dark:bg-black bg-white">
+    <div className="px-2 sm:px-10 min-h-screen w-screem flex items-center justify-center text-black dark:text-white dark:bg-black bg-white">
       <div>
-        <h2 className="hero-h2 text-6xl">So We Are Here</h2>
-        <p className="hero-p max-w-[50vw] text-lg">
+        <h2 className="hero-h2 text-6xl">So We Are Here Again</h2>
+        <p className="hero-p sm:max-w-[50vw] text-lg my-2">
           Lorem ipsum dolor sit amet consectetur, adipisicing elit. Totam
           aliquid labore molestiae! Dolores facilis nostrum quod sunt magnam,
-          optio rem? Lorem ipsum dolor sit amet consectetur adipisicing elit. Impedit, consequatur?
+          optio rem? Lorem ipsum dolor sit amet consectetur adipisicing elit.
+          Impedit, consequatur?
         </p>
+        <button className="border border-opacity-0 hero-btn px-10 py-2 text-2xl rounded-full bg-slate-300 shadow-xl drop-shadow-2xl">
+          Got It
+        </button>
       </div>
     </div>
   );
